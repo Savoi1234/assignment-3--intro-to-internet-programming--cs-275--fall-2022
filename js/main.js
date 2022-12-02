195e7a6d663162981fc0d837644276ec9b36f89f
Savoi1234 <112000562+Savoi1234@users.noreply.github.com>
2022-12-01 21:41:45 -0500

    creating container 4 data output

---
+++ js/main.js
@@ -0,0 +1,8 @@
+unction connector(json) {
+    let sldeOne = document.querySelectorAll(`div`)[1];//<-AQUIRING DATA FOR SLIDES
+ 
+    for (let i = 0; i < json.length; i++) {
+        let div = document.createElement(`float`);
+        div.classList = `content`;
+    }
+}
