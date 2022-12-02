9e895fe859183ae60245cd14279bbd3bf69a7b80
Savoi1234 <112000562+Savoi1234@users.noreply.github.com>
2022-12-01 21:42:37 -0500

    adds div data info

--- js/main.js
+++ js/main.js
@@ -4,5 +4,17 @@ unction connector(json) {
     for (let i = 0; i < json.length; i++) {
         let div = document.createElement(`float`);
         div.classList = `content`;
+        div.innerHTML = `<div class="details">
+        <h2>`+(json[i].album)+`</h2>
+        <a href="`+(json[i].url)+`">`+(json[i].artist)+`</a></div>
+        <img class="details" src="`+(json[i].cover_image.path)+
+        `" width="`+(json[i].cover_image.width)+`"height="`+(json[i].cover_image.height)+
+        `"alt="`+(json[i].cover_image.alt_content)+`"><p class="middlecred"> credit: <a href ="${json[i].cover_image.url}">${json[i].artist}</a></p>
+        <p class= "paragraph">`+(json[i].review.content)+`</p>
+        <p>—<a href="`+(json[i].review.url)+`">`
+        +(json[i].review.source)+`</a></p>`;
+ 
+         sldeOne.append(div);
+
     }
 }
